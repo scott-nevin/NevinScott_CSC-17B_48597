d0937dc15a111965d914a83e525cef5aa3d77f27
scott-nevin <scott.nevin@att.net>
2014-09-04 12:44:00 -0700

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- Lab/helloworldGUI/main.cpp
+++ Lab/helloworldGUI/main.cpp
@@ -2,7 +2,7 @@
  * Scott Nevin
  * August 27th, 2014
  * Purpose: Hello World GUI
-*/
+ */
 
 //Libraries
 #include <QApplication>
@@ -14,7 +14,7 @@
 int main(int argc, char **argv){
 
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("\n  Hello World!\n");
+    QLabel *label = new QLabel("\n       Hello World!\n");
     //label->show();
     (*label).show();
     return app.exec();
---
+++ Lab/print2Darray/main.cpp
@@ -0,0 +1,25 @@
+/*
+    Scott Nevin
+    09/04/14
+    Purpose: Print 2D array using classes and labels
+*/
+
+//Libraries
+#include <iostream>
+#include <cstdlib>
+#include <QApplication>
+#include <QLabel>
+using namespace std;
+
+//Global variables and constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char **argv){
+
+    cout<<"Hello World!"<<endl;
+
+    return 0;
+}
+
