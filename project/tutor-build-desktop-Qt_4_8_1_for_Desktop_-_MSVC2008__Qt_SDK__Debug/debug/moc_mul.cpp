da14cd18a1c4e136d44176d041b7eef7662f855b
scott-nevin <scott.nevin@att.net>
2014-12-10 18:44:09 -0800

    Signed-off-by: scott-nevin <scott.nevin@att.net>

--- project/tutor-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2008__Qt_SDK__Debug/debug/moc_mul.cpp
+++ project/tutor-build-desktop-Qt_4_8_1_for_Desktop_-_MSVC2008__Qt_SDK__Debug/debug/moc_mul.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mul.h'
 **
-** Created: Wed Dec 10 17:30:08 2014
+** Created: Wed Dec 10 18:06:05 2014
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
